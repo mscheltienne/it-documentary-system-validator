913d1f059adc59ab7a3bc51f4bb742af78958365
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-25 17:49:58 +0200

    remove leading underscore for files

--- itvalidator/conftest.py
+++ itvalidator/conftest.py
@@ -2,7 +2,6 @@ from __future__ import annotations  # c.f. PEP 563, PEP 649
 
 import random
 import string
-from datetime import datetime, timedelta
 from typing import TYPE_CHECKING
 from uuid import uuid4
 
@@ -46,27 +45,16 @@ def folder(tmp_path: Path) -> Path:
     return tmp_path
 
 
-def _random_datecode(datelimit: str | None = None) -> str:
-    """Create a random datecode"""
-    if datelimit is None:
-        year = random.randint(19, 25)
-        month = random.randint(1, 12)
-        day = random.randint(1, 28)
-        date = f"{year:02d}{month:02d}{day:02d}"
-    else:
-        datelimit = datetime.strptime(datelimit, "%y%m%d")
-        date = datelimit - timedelta(days=random.randint(1, 700))
-        date = date.strftime("%y%m%d")
-    return date
-
-
 def _random_name(code: str) -> str:
     """Create a random file/folder name."""
-    datecode = _random_datecode()
+    year = random.randint(19, 25)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)
+    datecode = f"{year:02d}{month:02d}{day:02d}"
     segments = random.randint(1, 3)
     name = "_".join(str(uuid4()).split("-")[:segments])
     usercode = "".join(random.sample(string.ascii_lowercase, 3)).upper()
-    return f"_{code}_{datecode}_{name}_{usercode}"
+    return f"{code}_{datecode}_{name}_{usercode}"
 
 
 def _create_files(folder: Path, code: str):
@@ -91,6 +79,6 @@ def _create_tree(folder: Path, code: str, depth: int):
     n_subfolders = random.randint(0, 3)
     for k in range(n_subfolders + 1):
         code_subfolder = f"{code}{string.ascii_lowercase[k]}"
-        curdir = folder / _random_name(code_subfolder)
+        curdir = folder / f"_{_random_name(code_subfolder)}"
         curdir.mkdir()
         _create_tree(curdir, code_subfolder, depth + 1)
