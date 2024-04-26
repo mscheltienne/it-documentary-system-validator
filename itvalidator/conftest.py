b1ebb2e4387bcf4682aa2867b9ead526d8e3a867
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-26 10:26:51 +0200

    cleaner test tree

--- itvalidator/conftest.py
+++ itvalidator/conftest.py
@@ -15,7 +15,7 @@ if TYPE_CHECKING:
     from pytest import Config
 
 
-_MAX_TREE_DEPTH: int = 4
+_MAX_TEST_TREE_DEPTH: int = 4
 
 
 def pytest_configure(config: Config) -> None:
@@ -37,24 +37,23 @@ def pytest_configure(config: Config) -> None:
 def folder(tmp_path: Path) -> Path:
     """Create a mock documentary structure."""
     for code in "F1", "F2", "F3":
-        segments = random.randint(1, 3)
-        name = "_".join(str(uuid4()).split("-")[:segments])
-        curdir = tmp_path / f"_{code}_{name}"
+        curdir = tmp_path / f"_{code}_{_random_stem()}"
         curdir.mkdir()
         _create_tree(curdir, code, depth=1)
     return tmp_path
 
 
-def _random_name(code: str) -> str:
-    """Create a random file/folder name."""
-    year = random.randint(19, 25)
-    month = random.randint(1, 12)
-    day = random.randint(1, 28)
-    datecode = f"{year:02d}{month:02d}{day:02d}"
-    segments = random.randint(1, 3)
-    name = "_".join(str(uuid4()).split("-")[:segments])
-    usercode = "".join(random.sample(string.ascii_lowercase, 3)).upper()
-    return f"{code}_{datecode}_{name}_{usercode}"
+def _create_tree(folder: Path, code: str, depth: int) -> None:
+    """Create a directory tree."""
+    _create_files(folder, code)
+    if depth == _MAX_TEST_TREE_DEPTH:
+        return
+    n_subfolders = random.randint(0, 3)
+    for k in range(n_subfolders + 1):
+        code_subfolder = f"{code}{string.ascii_lowercase[k]}"
+        curdir = folder / f"_{code_subfolder}_{_random_stem()}"
+        curdir.mkdir()
+        _create_tree(curdir, code_subfolder, depth + 1)
 
 
 def _create_files(folder: Path, code: str) -> None:
@@ -71,14 +70,18 @@ def _create_files(folder: Path, code: str) -> None:
             (folder / "__old" / fname).with_suffix(".txt").write_text("101")
 
 
-def _create_tree(folder: Path, code: str, depth: int) -> None:
-    """Create a directory tree."""
-    _create_files(folder, code)
-    if depth == _MAX_TREE_DEPTH:
-        return
-    n_subfolders = random.randint(0, 3)
-    for k in range(n_subfolders + 1):
-        code_subfolder = f"{code}{string.ascii_lowercase[k]}"
-        curdir = folder / f"_{_random_name(code_subfolder)}"
-        curdir.mkdir()
-        _create_tree(curdir, code_subfolder, depth + 1)
+def _random_stem() -> str:
+    """Create a random file/folder stem."""
+    segments = random.randint(1, 3)
+    return "_".join(str(uuid4()).split("-")[:segments])
+
+
+def _random_name(code: str) -> str:
+    """Create a random file name."""
+    year = random.randint(19, 25)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)
+    datecode = f"{year:02d}{month:02d}{day:02d}"
+    name = _random_stem()
+    usercode = "".join(random.sample(string.ascii_lowercase, 3)).upper()
+    return f"{code}_{datecode}_{name}_{usercode}"
