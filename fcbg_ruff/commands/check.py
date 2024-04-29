d6a40f268df779866bb0e0293a45d3b00646d381
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-29 15:09:39 +0200

    add entry-point

---
+++ fcbg_ruff/commands/check.py
@@ -0,0 +1,24 @@
+import argparse
+
+from ..check import validate_folder
+
+
+def run() -> None:
+    """Run check() command."""
+    parser = argparse.ArgumentParser(
+        prog=f"{__package__.split('.')[0]}-check", description="check"
+    )
+    parser.add_argument(
+        "folder",
+        type=str,
+        help="path to the folder to validate.",
+    )
+    parser.add_argument(
+        "--jobs",
+        type=int,
+        metavar="int",
+        help="number of jobs running in parallel.",
+        default=1,
+    )
+    args = parser.parse_args()
+    validate_folder(args.folder, args.jobs)
